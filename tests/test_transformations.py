0fc9836142c1c0e3d3e98bf50519eda24fe4aabd
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-07-24 20:25:59 -0700

    🔥 Delete some columns

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import urbanicola_data as dt
 
 
@@ -7,3 +8,11 @@ def test_add_offset():
     expected = augend + addend
     obtained = dt.add_offset(augend, addend)
     assert expected == obtained
+
+all_sales = pd.read_csv("/workdir/tests/data/venta.csv")
+
+
+def test_sales() -> None:
+    row: dict = all_sales.to_dict("records")[1]
+    new_row: dict = {k: [v] for k, v in row.items()}
+    dt.Sales(**new_row)
